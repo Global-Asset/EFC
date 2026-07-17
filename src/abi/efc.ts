7ecbf57bc9fc1666408b0901fd6b06b1e9461f4d
Efikcoin wallet <Support@efikcoin.xyz>
2026-07-17 02:05:16 +0200

    Add EFC_ABI for ERC20 token functions

---
+++ src/abi/efc.ts
@@ -0,0 +1,26 @@
+export const EFC_ABI = [
+  // balanceOf
+  {
+    constant: true,
+    inputs: [{ name: "owner", type: "address" }],
+    name: "balanceOf",
+    outputs: [{ name: "balance", type: "uint256" }],
+    type: "function",
+  },
+  // decimals
+  {
+    constant: true,
+    inputs: [],
+    name: "decimals",
+    outputs: [{ name: "", type: "uint8" }],
+    type: "function",
+  },
+  // symbol
+  {
+    constant: true,
+    inputs: [],
+    name: "symbol",
+    outputs: [{ name: "", type: "string" }],
+    type: "function",
+  },
+];
