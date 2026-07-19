805dd124e032dcd7131ee7344f8554f8c113964e
Efikcoin wallet <Support@efikcoin.xyz>
2026-07-19 10:24:23 +0200

    Add EFC_TOKEN and BSC_RPC constants

---
+++ src/lib/efc.ts
@@ -0,0 +1,11 @@
+export const EFC_TOKEN = {
+  name: "Efikcoin",
+  symbol: "EFC",
+  network: "BNB Smart Chain",
+  contract:
+    "0x677ce9cba67f7484ea951a12897ce780cfd8fed1",
+  decimals: 18,
+};
+
+export const BSC_RPC =
+  "https://bsc-dataseed.binance.org/";
