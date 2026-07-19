65048c118d09cbced1f417806b6e5954400ed0a9
Efikcoin wallet <Support@efikcoin.xyz>
2026-07-19 10:18:49 +0200

    Add Home component for Efikcoin payment system

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Efikcoin EFC Pay</h1>
+
+      <p>
+        Welcome to the Efikcoin merchant payment system.
+      </p>
+
+      <p>
+        Accept EFC payments anywhere in the world.
+      </p>
+
+      <h2>
+        EFC Contract Address
+      </h2>
+
+      <p>
+        0x677ce9cba67f7484ea951a12897ce780cfd8fed1
+      </p>
+    </main>
+  );
+}
